dd4bb1aa754ef73feeb55f826424da9bf6f9df03
zikiofficial <kondratenkomaksim234@gmail.com>
2024-08-31 14:39:12 +0500

    1.0

---
+++ NewContextMenu.bat
@@ -0,0 +1,4 @@
+@echo off
+cd C:\Windows\System32
+reg.exe delete "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}" /f
+taskkill /F /IM explorer.exe & start explorer
---
+++ OldContextMenu.bat
@@ -0,0 +1,4 @@
+@echo off
+cd C:\Windows\System32
+reg.exe add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f
+taskkill /F /IM explorer.exe & start explorer
